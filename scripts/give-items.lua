24063cb32a5e9cf3bfc801ccf1faf52f0a822b1b
Ozy-Viking <admin@hankin.io>
2025-09-29 14:59:11 +1000

    give-items

--- scripts/give-items.lua
+++ scripts/give-items.lua
@@ -22,34 +22,43 @@ GiveItems.CreateGlobals = function()
 end
 
 GiveItems.OnLoad = function()
-    CommandsUtils.Register("muppet_streamer_v2_give_player_weapon_ammo", { "api-description.muppet_streamer_v2_give_player_weapon_ammo" }, GiveItems.GivePlayerWeaponAmmoCommand, true)
+    CommandsUtils.Register("muppet_streamer_v2_give_player_weapon_ammo",
+        {"api-description.muppet_streamer_v2_give_player_weapon_ammo"}, GiveItems.GivePlayerWeaponAmmoCommand, true)
     EventScheduler.RegisterScheduledEventType("GiveItems.GiveWeaponAmmoScheduled", GiveItems.GiveWeaponAmmoScheduled)
     MOD.Interfaces.Commands.GiveItems = GiveItems.GivePlayerWeaponAmmoCommand
 end
 
 ---@param command CustomCommandData
 GiveItems.GivePlayerWeaponAmmoCommand = function(command)
-    local commandData = CommandsUtils.GetSettingsTableFromCommandParameterString(command.parameter, true, CommandName, { "delay", "target", "weaponType", "forceWeaponToSlot", "selectWeapon", "ammoType", "ammoCount", "suppressMessages" })
+    local commandData = CommandsUtils.GetSettingsTableFromCommandParameterString(command.parameter, true, CommandName,
+        {"delay", "target", "weaponType", "forceWeaponToSlot", "selectWeapon", "ammoType", "ammoCount",
+         "suppressMessages"})
     if commandData == nil then
         return
     end
 
     local delaySeconds = commandData.delay
-    if not CommandsUtils.CheckNumberArgument(delaySeconds, "double", false, CommandName, "delay", 0, nil, command.parameter) then
+    if not CommandsUtils.CheckNumberArgument(delaySeconds, "double", false, CommandName, "delay", 0, nil,
+        command.parameter) then
         return
     end ---@cast delaySeconds double|nil
-    local scheduleTick = Common.DelaySecondsSettingToScheduledEventTickValue(delaySeconds, command.tick, CommandName, "delay")
+    local scheduleTick = Common.DelaySecondsSettingToScheduledEventTickValue(delaySeconds, command.tick, CommandName,
+        "delay")
 
     local target = commandData.target
     if not Common.CheckPlayerNameSettingValue(target, CommandName, "target", command.parameter) then
         return
     end ---@cast target string
 
-    local weaponPrototype, valid = Common.GetItemPrototypeFromCommandArgument(commandData.weaponType, "gun", false, CommandName, "weaponType", command.parameter)
-    if not valid then return end
+    local weaponPrototype, valid = Common.GetItemPrototypeFromCommandArgument(commandData.weaponType, "gun", false,
+        CommandName, "weaponType", command.parameter)
+    if not valid then
+        return
+    end
 
     local forceWeaponToSlot = commandData.forceWeaponToSlot
-    if not CommandsUtils.CheckBooleanArgument(forceWeaponToSlot, false, CommandName, "forceWeaponToSlot", command.parameter) then
+    if not CommandsUtils.CheckBooleanArgument(forceWeaponToSlot, false, CommandName, "forceWeaponToSlot",
+        command.parameter) then
         return
     end ---@cast forceWeaponToSlot boolean|nil
     if forceWeaponToSlot == nil then
@@ -64,16 +73,21 @@ GiveItems.GivePlayerWeaponAmmoCommand = function(command)
         selectWeapon = false
     end
 
-    local ammoPrototype, valid = Common.GetItemPrototypeFromCommandArgument(commandData.ammoType, "ammo", false, CommandName, "ammoType", command.parameter)
-    if not valid then return end
+    local ammoPrototype, valid = Common.GetItemPrototypeFromCommandArgument(commandData.ammoType, "ammo", false,
+        CommandName, "ammoType", command.parameter)
+    if not valid then
+        return
+    end
 
     local ammoCount = commandData.ammoCount
-    if not CommandsUtils.CheckNumberArgument(ammoCount, "int", false, CommandName, "ammoCount", 1, MathUtils.uintMax, command.parameter) then
+    if not CommandsUtils.CheckNumberArgument(ammoCount, "int", false, CommandName, "ammoCount", 1, MathUtils.uintMax,
+        command.parameter) then
         return
     end ---@cast ammoCount uint|nil
 
     local suppressMessages = commandData.suppressMessages
-    if not CommandsUtils.CheckBooleanArgument(suppressMessages, false, CommandName, "suppressMessages", command.parameter) then
+    if not CommandsUtils.CheckBooleanArgument(suppressMessages, false, CommandName, "suppressMessages",
+        command.parameter) then
         return
     end ---@cast suppressMessages boolean|nil
     if suppressMessages == nil then
@@ -82,12 +96,26 @@ GiveItems.GivePlayerWeaponAmmoCommand = function(command)
 
     storage.giveItems.nextId = storage.giveItems.nextId + 1
     ---@type GiveItems_GiveWeaponAmmoScheduled
-    local giveWeaponAmmoScheduled = { target = target, ammoPrototype = ammoPrototype, ammoCount = ammoCount, weaponPrototype = weaponPrototype, forceWeaponToSlot = forceWeaponToSlot, selectWeapon = selectWeapon, suppressMessages = suppressMessages }
+    local giveWeaponAmmoScheduled = {
+        target = target,
+        ammoPrototype = ammoPrototype,
+        ammoCount = ammoCount,
+        weaponPrototype = weaponPrototype,
+        forceWeaponToSlot = forceWeaponToSlot,
+        selectWeapon = selectWeapon,
+        suppressMessages = suppressMessages
+    }
     if scheduleTick ~= -1 then
-        EventScheduler.ScheduleEventOnce(scheduleTick, "GiveItems.GiveWeaponAmmoScheduled", storage.giveItems.nextId, giveWeaponAmmoScheduled)
+        EventScheduler.ScheduleEventOnce(scheduleTick, "GiveItems.GiveWeaponAmmoScheduled", storage.giveItems.nextId,
+            giveWeaponAmmoScheduled)
     else
         ---@type UtilityScheduledEvent_CallbackObject
-        local eventData = { tick = command.tick, name = "GiveItems.GiveWeaponAmmoScheduled", instanceId = storage.giveItems.nextId, data = giveWeaponAmmoScheduled }
+        local eventData = {
+            tick = command.tick,
+            name = "GiveItems.GiveWeaponAmmoScheduled",
+            instanceId = storage.giveItems.nextId,
+            data = giveWeaponAmmoScheduled
+        }
         GiveItems.GiveWeaponAmmoScheduled(eventData)
     end
 end
@@ -102,17 +130,21 @@ GiveItems.GiveWeaponAmmoScheduled = function(eventData)
         return
     end
     if targetPlayer.controller_type ~= defines.controllers.character or targetPlayer.character == nil then
-        if not data.suppressMessages then game.print({ "message.muppet_streamer_v2_give_player_weapon_ammo_not_character_controller", data.target }) end
+        if not data.suppressMessages then
+            game.print({"message.muppet_streamer_v2_give_player_weapon_ammo_not_character_controller", data.target})
+        end
         return
     end
 
     -- Check the weapon and ammo are still valid (unchanged).
     if data.weaponPrototype ~= nil and not data.weaponPrototype.valid then
-        CommandsUtils.LogPrintWarning(CommandName, nil, "The in-game weapon prototype has been changed/removed since the command was run.", nil)
+        CommandsUtils.LogPrintWarning(CommandName, nil,
+            "The in-game weapon prototype has been changed/removed since the command was run.", nil)
         return
     end
     if data.ammoPrototype ~= nil and not data.ammoPrototype.valid then
-        CommandsUtils.LogPrintWarning(CommandName, nil, "The in-game ammo prototype has been changed/removed since the command was run.", nil)
+        CommandsUtils.LogPrintWarning(CommandName, nil,
+            "The in-game ammo prototype has been changed/removed since the command was run.", nil)
         return
     end
 
@@ -121,12 +153,19 @@ GiveItems.GiveWeaponAmmoScheduled = function(eventData)
         ammoName = data.ammoPrototype.name
     end
     if data.weaponPrototype ~= nil then
-        PlayerWeapon.EnsureHasWeapon(targetPlayer, data.weaponPrototype.name, data.forceWeaponToSlot, data.selectWeapon, ammoName)
+        PlayerWeapon.EnsureHasWeapon(targetPlayer, data.weaponPrototype.name, data.forceWeaponToSlot, data.selectWeapon,
+            ammoName)
     end
     if ammoName ~= nil then
-        local inserted = targetPlayer.insert({ name = ammoName, count = data.ammoCount })
+        local inserted = targetPlayer.insert({
+            name = ammoName,
+            count = data.ammoCount
+        })
         if inserted < data.ammoCount then
-            targetPlayer.surface.spill_item_stack(targetPlayer.position, { name = ammoName, count = data.ammoCount - inserted }, true, nil, false)
+            targetPlayer.physical_surface.spill_item_stack(targetPlayer.physical_position, {
+                name = ammoName,
+                count = data.ammoCount - inserted
+            }, true, nil, false)
         end
     end
 end
